0f529befe80dd3a1a8548d088c159b9850a0c03b
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-02-06 22:27:45 -0400

    feat: add names

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,10 @@ async fn main() {
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let local_addr = listener.local_addr().unwrap();
+    println!("->> LISTENING ON http://{:?}\n", local_addr);
+
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to initialize the API server");
 }
