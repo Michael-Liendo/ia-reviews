7bde3cee195b678df97b7162cf398b17bbc8019e
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-02-08 19:25:59 -0400

    feat: config database (#1)
    * Add migration files and dependencies
    
    * Remove todo! statements in migration script
    
    * chore: rename project and create entity
    
    * Add HOST and PORT configuration
    
    * Refactor code: Remove unused imports and add print statement for database connection
    
    * refactor: code to use new state struct
    
    * Update Cargo.toml and migration files
    
    * Update Cargo.toml to include correct path for workspace members
    
    * Add columns to the users table
    
    * Update dependencies in Cargo.lock and Cargo.toml
    
    * Add entity crate and dependencies
    
    * Add SeaORM entity for user

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::env;
+
 use axum::Router;
+use dotenv::dotenv;
+use sea_orm::Database;
 
 mod controllers;
 mod routes;
@@ -6,11 +10,26 @@ mod types;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let server_url = format!("{host}:{port}");
+
+    let conn = Database::connect(db_url)
+        .await
+        .expect("Database connection failed");
+    println!("->> DATABASE CONNECTED\n");
+
+    let state = types::state::AppState { conn };
+
     // build our application with a single route
-    let app = Router::new().nest("/api", routes::auth::authentication());
+    let app: Router = Router::new()
+        .nest("/api", routes::create_routes())
+        .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(server_url).await.unwrap();
     let local_addr = listener.local_addr().unwrap();
     println!("->> LISTENING ON http://{:?}\n", local_addr);
 
